024227da9b0a94bc68fd2036dcb134561a55e254
JesusAbreuGonzalez <77336425+JesusAbreuGonzalez@users.noreply.github.com>
2021-02-27 15:44:39 -0400

    Agregando clases de ejercicios del capitulo 12

---
+++ Tarea5/EjercicioCSharp/Capitulo12/Ejercicio12_1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EjercicioCSharp.Capitulo12
+{
+    public class Ejercicio12_1
+    {
+        public Ejercicio12_1()
+        {
+
+        }
+        
+        public int Factorial(int numero)
+        {
+            int resultado = 1;
+            for (int i = 1; i <= numero; i++)
+            {
+                resultado *= i;
+            }
+            return resultado;
+        }
+
+        public void Mostrar()
+        {
+            int numero;
+
+            Console.WriteLine("En este programa se mostrara el valor del factorial del numero que ingrese.");
+
+            Console.Write("Ingrese el numero: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"El factorial de {numero} es: {Factorial(numero)}");
+        }
+
+    }    
+}
